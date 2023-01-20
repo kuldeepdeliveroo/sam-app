0c87310e38c303f88033009b50c0fe01e8ff4dff
Kuldeep Yadav <kuldeep.yadav@deliveroo.com>
2023-01-20 15:03:57 +0530

    Working code 4

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"log"
 )
 
-func handler() {
+func handler() (events.APIGatewayProxyResponse, error) {
 	log.Printf("Hello duniya walo!! How are you doing?")
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello World",
+	}, nil
 }
 
 func main() {
