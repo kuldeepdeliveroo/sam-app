f9559b8fba46853ee39097f50da44a9b7cef2e00
Kuldeep Yadav <kuldeep.yadav@deliveroo.com>
2023-01-23 14:44:19 +0530

    Debug logs added

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,40 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"log"
+	"hello-world/handler"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Printf("Hello duniya walo!! How are you doing?")
-	log.Printf(
-		"Headers: %v",
-		marshal(request.Headers),
-	)
-
-	log.Printf(
-		"Pathparams: %v",
-		marshal(request.PathParameters),
-	)
-
-	log.Printf(
-		"Queryparams: %v",
-		marshal(request.QueryStringParameters),
-	)
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello World",
-	}, nil
-}
-
-func marshal(obj map[string]string) string {
-	res, _ := json.Marshal(obj)
-	return string(res)
-}
-
 func main() {
-	lambda.Start(handler)
+	lambda.Start(handler.Handle)
 }
